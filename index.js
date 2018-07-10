d64433ac8689937c6969403ce6e475c7b63f9080
Roig Rosa <roigrosa88@gmail.com>
2018-07-10 15:25:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 function dwarfRollCall(dwarves) {
+var result = "";
+for (var i = 0; i < dwarves.length; i = i +1){
+  result = result + (1+i) + ". " + dwarves[i]+ " "
+}
+return result;
 }
 
 function summonCaptainPlanet(planeteerCalls){
+var i = 0;
+var copyArray = new Array();
+  while (i < planeteerCalls.length){
+    copyArray[i] = planeteerCalls[i].toUpperCase() + "!";
+    i++
+  }
+  return copyArray;
 }
 
 function longPlaneteerCalls(words) {
+  for (var i = 0; i < words.length; i++){
+    if (words[i].length > 4)
+  {
+      return true;
+    }
+  }
+  return false;
 }
-
 function findTheCheese (foods) {
 }
